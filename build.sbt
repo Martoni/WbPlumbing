c02abc5ee4465110c4fba1bb69a925339e6167a2
Fabien Marteau <fabien.marteau@armadeus.com>
2019-11-06 10:57:57 +0100

    not working tester2 implementation for tests

--- build.sbt
+++ build.sbt
@@ -43,11 +43,13 @@ resolvers ++= Seq(
 // Provide a managed dependency on X if -DXVersion="" is supplied on the command line.
 val defaultVersions = Map(
   "chisel3" -> "3.2.+",
-  "chisel-iotesters" -> "1.3.+"
+  "chisel-iotesters" -> "1.3.+",
   )
 
 libraryDependencies ++= Seq("chisel3","chisel-iotesters").map {
 dep: String => "edu.berkeley.cs" %% dep % sys.props.getOrElse(dep + "Version", defaultVersions(dep)) }
+
+libraryDependencies += "edu.berkeley.cs" %% "chisel-testers2" % "0.1-SNAPSHOT"
 
 scalacOptions ++= Seq("-deprecation", "-feature")
 
--- src/test/scala/wbplumbing/testwbplumbing.scala
+++ src/test/scala/wbplumbing/testwbplumbing.scala
@@ -1,10 +1,10 @@
 package wbplumbing
 
+import org.scalatest.{Matchers, FlatSpec}
 import chisel3._
+import chisel3.tester._
 import chisel3.util._
 import chisel3.experimental._
-import chisel3.iotesters.PeekPokeTester
-import org.scalatest.{Matchers, FlatSpec}
 
 object general {
   val optn = Array("-td", "output",
@@ -13,70 +13,8 @@ object general {
                   )
 }
 
-class TestWbIntercon (dut: WbInterconPT) extends PeekPokeTester(dut){
-  // just a stupid testbench that verify point to point connexion
-  poke(dut.io.wbm.adr_o, 1)
-  poke(dut.io.wbm.dat_o, 1)
-  poke(dut.io.wbm.we_o , 1)
-  poke(dut.io.wbm.stb_o, 1)
-  poke(dut.io.wbm.cyc_o, 1)
-  poke(dut.io.wbs.ack_o, 1)
-  poke(dut.io.wbs.dat_o, 1)
-  step(1)
-  expect(dut.io.wbm.adr_o, 1, "Wrong value read on wbm.adr_o")
-  expect(dut.io.wbm.dat_o, 1, "Wrong value read on wbm.dat_o")
-  expect(dut.io.wbm.we_o , 1, "Wrong value read on wbm.we_o ")
-  expect(dut.io.wbm.stb_o, 1, "Wrong value read on wbm.stb_o")
-  expect(dut.io.wbm.cyc_o, 1, "Wrong value read on wbm.cyc_o")
-  expect(dut.io.wbs.ack_o, 1, "Wrong value read on wbs.ack_o")
-  expect(dut.io.wbs.dat_o, 1, "Wrong value read on wbs.dat_o")
-  poke(dut.io.wbm.adr_o, 0)
-  poke(dut.io.wbm.dat_o, 0)
-  poke(dut.io.wbm.we_o , 0)
-  poke(dut.io.wbm.stb_o, 0)
-  poke(dut.io.wbm.cyc_o, 0)
-  poke(dut.io.wbs.ack_o, 0)
-  poke(dut.io.wbs.dat_o, 0)
-  step(1)
-  expect(dut.io.wbm.adr_o, 0, "Wrong value read on wbm.adr_o")
-  expect(dut.io.wbm.dat_o, 0, "Wrong value read on wbm.dat_o")
-  expect(dut.io.wbm.we_o , 0, "Wrong value read on wbm.we_o ")
-  expect(dut.io.wbm.stb_o, 0, "Wrong value read on wbm.stb_o")
-  expect(dut.io.wbm.cyc_o, 0, "Wrong value read on wbm.cyc_o")
-  expect(dut.io.wbs.ack_o, 0, "Wrong value read on wbs.ack_o")
-  expect(dut.io.wbs.dat_o, 0, "Wrong value read on wbs.dat_o")
-}
 
-class TestWbInterconDualSlave (dut: WbInterconOneMaster) extends PeekPokeTester(dut){
-  val firstSlave = 0
-  val secondSlave = 4
-  step(1)
-  // write on second slave
-  val value = 0xcafe
-  poke(dut.io.wbm.adr_o, secondSlave)
-  poke(dut.io.wbm.dat_o, value)
-  poke(dut.io.wbm.we_o, 1)
-  poke(dut.io.wbm.cyc_o, 1)
-  poke(dut.io.wbm.stb_o, 1)
-// Doesn't work 
-  for(wbs <- dut.io.wbs) {
-    wbs.ack_o := 0.U
-  }
-  step(1)
-  poke(dut.io.wbm.adr_o, 0)
-  poke(dut.io.wbm.dat_o, 0)
-  poke(dut.io.wbm.we_o, 0)
-  poke(dut.io.wbm.cyc_o, 0)
-  poke(dut.io.wbm.stb_o, 0)
-  poke(dut.io.wbs(1).ack_o, 1)
-  expect(dut.io.wbs(1).we_i, 1)
-  expect(dut.io.wbs(1).cyc_i, 1)
-  step(1)
-  poke(dut.io.wbs(0).ack_o, 0)
-  poke(dut.io.wbs(1).ack_o, 0)
-}
-
-class WbInterconPTSpec extends FlatSpec with Matchers {
+class WbInterconPTSpec extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "A WbInterconPT"
 
   it should "read and write wishbone value on one slave" in {
@@ -84,13 +22,44 @@ class WbInterconPTSpec extends FlatSpec with Matchers {
     val dataWidth = 16
     val wbm = new WbMaster(dataWidth, 2)
     val wbs = new WbSlave(dataWidth, 2)
-    chisel3.iotesters.Driver.execute(args, () => new WbInterconPT(wbm, wbs))
-          {c => new TestWbIntercon(c)} should be(true)
 
+    test(new WbInterconPT(wbm, wbs)){ dut =>
+      // just a stupid testbench that verify point to point connexion
+      dut.io.wbm.adr_o.poke(1)
+      dut.io.wbm.dat_o.poke(1)
+      dut.io.wbm.we_o.poke (1)
+      dut.io.wbm.stb_o.poke(1)
+      dut.io.wbm.cyc_o.poke(1)
+      dut.io.wbs.ack_o.poke(1)
+      dut.io.wbs.dat_o.poke(1)
+      dut.clock.step(1)
+      dut.io.wbm.adr_o.expect(1, "Wrong value read on wbm.adr_o")
+      dut.io.wbm.dat_o.expect(1, "Wrong value read on wbm.dat_o")
+      dut.io.wbm.we_o.expect( 1, "Wrong value read on wbm.we_o ")
+      dut.io.wbm.stb_o.expect(1, "Wrong value read on wbm.stb_o")
+      dut.io.wbm.cyc_o.expect(1, "Wrong value read on wbm.cyc_o")
+      dut.io.wbs.ack_o.expect(1, "Wrong value read on wbs.ack_o")
+      dut.io.wbs.dat_o.expect(1, "Wrong value read on wbs.dat_o")
+      dut.io.wbm.adr_o.poke(0)
+      dut.io.wbm.dat_o.poke(0)
+      dut.io.wbm.we_o.poke( 0)
+      dut.io.wbm.stb_o.poke(0)
+      dut.io.wbm.cyc_o.poke(0)
+      dut.io.wbs.ack_o.poke(0)
+      dut.io.wbs.dat_o.poke(0)
+      dut.clock.step(1)
+      dut.io.wbm.adr_o.expect(0, "Wrong value read on wbm.adr_o")
+      dut.io.wbm.dat_o.expect(0, "Wrong value read on wbm.dat_o")
+      dut.io.wbm.we_o.expect( 0, "Wrong value read on wbm.we_o ")
+      dut.io.wbm.stb_o.expect(0, "Wrong value read on wbm.stb_o")
+      dut.io.wbm.cyc_o.expect(0, "Wrong value read on wbm.cyc_o")
+      dut.io.wbs.ack_o.expect(0, "Wrong value read on wbs.ack_o")
+      dut.io.wbs.dat_o.expect(0, "Wrong value read on wbs.dat_o")
+    }
   }
 }
 
-class WbInterconOneMasterSpec extends FlatSpec with Matchers {
+class WbInterconOneMasterSpec extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "A WbInterconOneMaster"
 
   it should "read and write wishbone value on two slaves" in {
@@ -100,8 +69,34 @@ class WbInterconOneMasterSpec extends FlatSpec with Matchers {
     val wbs1 = new WbSlave(dataWidth, 2, "Ksz1")
     val wbs2 = new WbSlave(dataWidth, 2, "Ksz2")
 
-    chisel3.iotesters.Driver.execute(args, () => new WbInterconOneMaster(wbm, Seq(wbs1, wbs2)))
-          {c => new TestWbInterconDualSlave(c)} should be(true)
+    test(new WbInterconOneMaster(wbm, Seq(wbs1, wbs2))) { dut =>
+      val firstSlave = 0
+      val secondSlave = 4
+      step(1)
+      // write on second slave
+      val value = 0xcafe
+      dut.io.wbm.adr_o.poke(secondSlave)
+      dut.io.wbm.dat_o.poke(value)
+      dut.io.wbm.we_o.poke(1)
+      dut.io.wbm.cyc_o.poke(1)
+      dut.io.wbm.stb_o.poke(1)
+      for(wbs <- dut.io.wbs) {
+        wbs.ack_o.poke(0.U)
+      }
+      dut.clock.step(1)
+      dut.io.wbm.adr_o.poke(0)
+      dut.io.wbm.dat_o.poke(0)
+      dut.io.wbm.we_o.poke(0)
+      dut.io.wbm.cyc_o.poke(0)
+      dut.io.wbm.stb_o.poke(0)
+      dut.io.wbs(1).ack_o.poke(1)
 
+      dut.io.wbs(1).we_i.expect(1)
+      dut.io.wbs(1).cyc_i.expect(1)
+      dut.clock.step(1)
+      dut.io.wbs(0).ack_o.poke(0)
+      dut.io.wbs(1).ack_o.poke(0)
+
+    }
   }
 }
